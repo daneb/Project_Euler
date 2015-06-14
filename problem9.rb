3feb384884cad8db5419ceec088b72063fb2499e
daneb <dane.balia@hetzner.co.za>
2015-06-14 21:34:47 +0200

    Problem 9 solved - pythogorean triple

---
+++ problem9.rb
@@ -0,0 +1,31 @@
+require 'byebug'
+
+def pythagorean_triple(a, b)
+	square = Math.sqrt((a * a) + (b * b))
+	return nil if ((square - square.round) != 0)
+	square.round	
+end
+
+def determine_triplet(a, b=nil)
+	b = a if b.nil?
+	c = pythagorean_triple(a, b)
+	return -1 if (b >= 1000)
+	return -1 if ((not c.nil?) && (a + b + c > 1000))
+	puts "#{a} + #{b} + #{c}"
+ 	return a * b * c if ((not c.nil?) && (a + b + c == 1000))
+	determine_triplet(a, (b + 1))
+end
+
+# Lowest pythorean triple is 3,4,5
+(3..200).each { |n|
+  result = determine_triplet(n)
+  if (result > 0)
+	puts "Got it #{result}"
+	break
+  else
+  	puts 'Nothing found'
+  end
+}
+
+
+
