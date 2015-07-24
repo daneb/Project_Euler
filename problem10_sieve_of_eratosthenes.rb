bed3d8284b26921391cc51aca345147c1aead544
daneb <dane.balia@hetzner.co.za>
2015-07-24 07:25:06 +0200

    YAY - working sieve of eratosthenes

--- problem10_sieve_of_eratosthenes.rb
+++ problem10_sieve_of_eratosthenes.rb
@@ -1,21 +1,26 @@
-array = Array.new
-upperbound = 2000000
-composite = Math.sqrt(2000000).round
+require 'prime'
 
-(2..upperbound).step(1) do 
-  array << false
+array = Array.new
+upperbound = 2_000_000 - 1
+composite = Math.sqrt(upperbound).floor
+result = 0
+
+(2..upperbound + 1).step(1) do 
+  array << true
 end
 
-(2..composite).step(1) do |m|
-  if array[m]
-    ((k = m ** 2)..upperbound).step(k = k + m) do |x|
-      array[x] = true
+(2..composite).step(1) do |i|
+  if array[i] == true
+    (i*i..upperbound).step(i) do |x| 
+      array[x] = false
     end
   end
 end
 
-(composite..upperbound).step(1) do |m|
-  if array[m] == false
-    puts (m)
+(2..upperbound).step(1) do |n|
+  if array[n] == true
+    result = result + n
   end
 end
+
+puts "Sum of price numbers for < 2_000_000 = #{result}"
