cd0f7ae99d03debb9ff48d4762b6017f1e035a3e
daneb <dane.balia@hetzner.co.za>
2016-05-12 07:57:11 +0200

    Starting Problem 11

---
+++ problem11.rb
@@ -0,0 +1,6 @@
+# determine how to make a grid
+# the pattern but sounds too intensive would be 
+# to find the 0,0 - then to go to row 1,1.
+# then to go to row 2,2
+# then go to row 3,3
+# rinse and repeat. 
